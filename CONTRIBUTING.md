35e3d2465dcb93902ecc75bafb403e55289e7975
Pierre PIRONIN <pierre@pierrepironin.fr>
2018-11-05 15:24:31 +0100

    :sparkles: Generate an empty app directory given user inputs.

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -6,8 +6,10 @@ Welcome to Imerologio CLI project !
 Imerologio CLI is a Go application so make sure your have a development environment with Go 1.2+.  
 [See the install instructions for Go](http://golang.org/doc/install.html).
 
-### ishell
-This application is based on [ishell](https://github.com/abiosoft/ishell) to ensure an interactive CLI client, enjoy !
+### Dependencies
+Install the following dependencies with `go get XXX`:
+- [ishell](https://github.com/abiosoft/ishell) to ensure an interactive CLI : `go get github.com/fatih/color`
+- [Color](https://github.com/fatih/color) in shell : `go get github.com/fatih/color`
 
 ## About releasing
 This product is built using [goreleaser](https://goreleaser.com/), take a look at the config at [.goreleaser.yml](.goreleaser.yml).  
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "strings"
-import "github.com/abiosoft/ishell"
+import (
+	"fmt"
+	"github.com/abiosoft/ishell"
+	"github.com/fatih/color"
+	"log"
+	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
+)
+
+var promptColor = color.New(color.FgCyan).SprintfFunc()
+var errorColor = color.New(color.FgRed).SprintfFunc()
+var infoColor = color.New(color.FgMagenta).SprintfFunc()
 
 func main() {
 	// create new shell.
@@ -9,17 +21,101 @@ func main() {
 	shell := ishell.New()
 
 	// display welcome info.
-	shell.Println("Sample Interactive Shell")
+
+	shell.Println(infoColor("Imerologio CLI helps you bootstrap your event sourcing app easily ✨"))
 
 	// register a function for "greet" command.
 	shell.AddCmd(&ishell.Cmd{
-		Name: "greet",
-		Help: "greet user",
-		Func: func(c *ishell.Context) {
-			c.Println("Hello", strings.Join(c.Args, " "))
-		},
+		Name: "imerologio",
+		Help: "Create a new imerologio application",
+		Func: imerologio,
 	})
 
-	// run shell
-	shell.Run()
+	// when started with "exit" as first argument, assume non-interactive execution
+	shell.Process("imerologio")
+}
+
+func imerologio(c *ishell.Context) {
+	c.ShowPrompt(false)
+
+	appName := getAppName(c)
+	appPath := getAppPath(c, appName)
+
+	c.Println("")
+	c.Println(infoColor("Ok, let's recap"))
+	c.Println("----------------")
+	c.Print(promptColor("App name : "))
+	c.Println(appName)
+	c.Print(promptColor("App path : "))
+	c.Println(appPath)
+	c.Println("----------------")
+	for {
+		c.Print(infoColor("Launch generation ? [y/n]: "))
+		goGeneration := c.ReadLine()
+		if strings.ToLower(goGeneration) == "n" || strings.ToLower(goGeneration) == "no" {
+			c.Print("Ok, I've done nothing. See you soon 👋")
+			break
+		} else if strings.ToLower(goGeneration) == "y" || strings.ToLower(goGeneration) == "yes" {
+			c.ProgressBar().Start()
+			for i := 0; i < 101; i++ {
+				c.ProgressBar().Suffix(fmt.Sprint(" ", i, "%"))
+				c.ProgressBar().Progress(i)
+				generateApp(appName, appPath)
+			}
+			c.ProgressBar().Stop()
+			c.Println("")
+			c.Print("Happy coding 🎉")
+			break
+		}
+		c.Println("Only y, yes, n or no are allowed")
+	}
+}
+
+func getAppName(c *ishell.Context) string {
+	for {
+		c.Print(promptColor("App name: "))
+		result := c.ReadLine()
+		if result != "" {
+			return result
+		}
+		c.Println(errorColor("App name must be a non empty string"))
+	}
+}
+
+func getAppPath(c *ishell.Context, appName string) string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal("Error while retrieving user's home directory")
+	}
+	suggestedPath := filepath.Join(usr.HomeDir, appName)
+
+	for {
+		c.Print(promptColor("Path [%s]: ", suggestedPath))
+		var appPath = c.ReadLine()
+
+		// user chose the suggested path
+		if appPath == "" {
+			return suggestedPath
+		}
+
+		basePath := filepath.Base(appPath)
+		// if user omitted the app name, let add it for him
+		if basePath != appName {
+			appPath = filepath.Join(appPath, appName)
+		}
+
+		// check that parent folder exists and that app folder does not exist to erase nothing
+		parentPath := filepath.Dir(appPath)
+		if _, err := os.Stat(parentPath); os.IsNotExist(err) {
+			c.Println(errorColor("The parent folder must exist and %s does not", parentPath))
+		} else if _, err := os.Stat(appPath); !os.IsNotExist(err) {
+			c.Println(errorColor("The given folder must be empty and %s is not", appPath))
+		} else {
+			return appPath
+		}
+	}
+}
+
+func generateApp(appName string, appPath string) {
+	os.Mkdir(appPath, os.ModeDir)
 }
